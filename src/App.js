7f3bee61deef20f7849596e0fa79c7352b0a321f
Stephanie Merle <merle.stephany@gmail.com>
2019-11-08 13:46:20 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  // default messages when launched
+  // should be replaced with local mongoDB data
+  const init = [
+    { message: "mon message", crossed: false },
+    { message: "mon message 2", crossed: true }
+  ];
+  // the state with all my TODO items
+  const [state, setState] = useState(init);
+  // the state to get the new input value
+  const [newInput, setInput] = useState("");
+
+  // function called when a list item is clicked
+  // should toggle the crossed state
+  const cross = i => {
+    let copy = [...state];
+    let crossStatus = copy[i].crossed;
+    copy[i].crossed = !crossStatus;
+    setState(copy);
+    console.log(state);
+  };
+  // function to delete item from the list
+  // const delete = i => {
+  //   console.log(i);
+  //   let newState = state.filter(el => el !== state[i]);
+  //   setState(newState);
+  //   console.log(state);
+  // };
+  // create a list item for each state object with a onClick call
+  const listing = state.map((el, index) => (
+    <li
+      key={index}
+      onClick={() => {
+        cross(index);
+      }}
+      className={el.crossed ? "crossed" : null}
+    >
+      {el.message}
+    </li>
+  ));
+  // function to add the new input into the state if not empty
+  const addTodoHandler = () => {
+    if (newInput) {
+      setState([...state, { message: newInput, crossed: false }]);
+      setInput("");
+    }
+  };
+  // update the new input state
+  const inputHandler = e => {
+    return setInput(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TO DO LIST</h1>
+      <div className="list">LIST</div>
+      <ul>{listing}</ul>
+
+      <input type="text" value={newInput} onChange={inputHandler} />
+      <div>
+        <button onClick={addTodoHandler}>ADD A TODO</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
